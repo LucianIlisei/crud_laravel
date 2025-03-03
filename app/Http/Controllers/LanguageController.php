cd43477ab035da3da064e5461173d2e015fc75bb
Lucian Ilisei <lucianioanilisei.trabajos@gmail.com>
2025-03-03 22:08:37 +0100

    Problema de los lenguajes resuelto

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -3,17 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Http\RedirectResponse;
 
 class LanguageController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
-    public function __invoke(Request $request, string $lang)
-
+    public function change($locale): RedirectResponse
     {
-        session()->put('locale', $lang);
+        if (!array_key_exists($locale, config('languages'))) {
+            abort(400, 'Idioma no válido');
+        }
+
+        Session::put('locale', $locale);
+        App::setLocale($locale);
         return redirect()->back();
-        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\ProyectoController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/language/{locale}', [LanguageController::class, 'change'])->name('language.change');
+
+
 Route::get("main", MainController::class);
 
 Route::middleware(['auth'])->group(function () {
@@ -25,7 +28,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get("language/{locale}", [LanguageController::class, 'change'])->name('language');
 
 require __DIR__.'/auth.php';
